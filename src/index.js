de9d8664e627acb7b59be7ab26e2be03259642e4
rjacks02 <rjacks02@villanova.edu>
2024-05-15 12:55:28 -0400

    added basename to hashrouter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
       <h1 class = 'Main'>The Kelly Criterion Investment Game</h1>
-    <HashRouter>
+    <HashRouter basename = '/KellyCriterion'>
     <Routes>
       <Route index element={<Intro />} />
       <Route path = 'Intro' element={<Intro />} />
