ae3574ced2bd5d63f5b409b6057de84d48c01f2b
rjacks02 <rjacks02@villanova.edu>
2024-05-15 12:47:53 -0400

    switch to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import Intro from './pages/Intro.js';
 import Game from './pages/Game.js';
 import End from './pages/End.js';
@@ -10,13 +10,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
       <h1 class = 'Main'>The Kelly Criterion Investment Game</h1>
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route index element={<Intro />} />
       <Route path = 'Intro' element={<Intro />} />
       <Route path = 'Game' element={<Game />} />
       <Route path = 'End' element={<End />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
   </div>
 );
